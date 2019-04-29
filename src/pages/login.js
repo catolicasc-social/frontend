3a9b829a6a3b3843cd9a209810fe70fa0a9362dd
Eduardo <eduardo.klein@catolicasc.org.br>
2019-04-28 21:18:40 -0300

    Melhorando tela de login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,49 +1,75 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import Router from 'next/router';
+import { Icon } from 'evergreen-ui';
 
 import { Button } from '../components/commons/Button';
 import { Heading } from '../components/commons/Heading';
-import { Pane } from '../components/commons/Pane';
 
 const handleLogin = () => {
   Router.push('/index');
 };
 
 const Login = () => (
-  <div className="container div-centered">
-    <div className="div-welcome div-centered">
-      <div className="div-centered">
-        <Pane id="paneWelcome" height={300}>
-          <Heading>Bem Vindo!</Heading>
-        </Pane>
-        <Pane height={100}>
-          <Button
-            appearance="primary"
-            intent="success"
-            isLoading={false}
-            onClick={handleLogin}
-          >
-            Login
-          </Button>
-        </Pane>
+  <div className="container">
+    <div className="div-Center">
+      <figure>
+        <Icon
+          icon="person"
+          size={35}
+          color="muted"
+          display="block"
+          margin="auto"
+        />
+      </figure>
+      <Heading>Bem vindo!</Heading>
+      <div className="div-Login">
+        <Button appearance="primary" isLoading={false} onClick={handleLogin}>
+          <Icon marginRight={16} icon="log-in" size={16} />
+          Login
+        </Button>
       </div>
     </div>
+    <footer>
+      Copyright &copy; 2019 Catolica - Joinville. All rights reserved.
+    </footer>
+    <style jsx global>
+      {`
+        html,
+        body {
+          height: 100% !important;
+          width: 100%;
+          overflow: hidden;
+          background-color: #f5f5f5;
+        }
+      `}
+    </style>
     <style jsx>
       {`
-        .div-centered {
+        .div-Login {
+          margin-top: 10vh;
           text-align: center;
-          padding-top: 5rem;
         }
-
+        .div-Center {
+          display: flex;
+          border-radius: 3rem;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          vertical-align: middle;
+          background-color: white;
+          margin: 25vh 25vw;
+          height: 40vh;
+          width: 25vw;
+        }
         .container {
+          width: 100%;
           height: 100%;
-        }
-
-        .div-welcome {
-          box-shadow: 2px 9px 8px 0px rgba(67, 90, 111, 0.3),
-            0 16px 24px -8px rgba(67, 90, 111, 0.47);
-          display: inline-table;
-          width: 50%;
-          height: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          min-height: 100%;
+          background-color: #f5f5f5;
         }
       `}
     </style>
