36f5acd4eb0c63d467a7cc6b1513663373c1d070
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-05-13 13:21:38 -0300

    Melhorando estilo da tela de login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,78 +1,79 @@
 import Router from 'next/router';
-import { Icon } from 'evergreen-ui';
 
+import { Pane } from '../components/commons/Pane';
 import { Button } from '../components/commons/Button';
 import { Heading } from '../components/commons/Heading';
+
+const YEAR = new Date().getFullYear();
 
 const handleLogin = () => {
   Router.push('/index');
 };
 
 const Login = () => (
-  <div className="container">
-    <div className="div-Center">
-      <figure>
-        <Icon
-          icon="person"
-          size={35}
-          color="muted"
-          display="block"
-          margin="auto"
-        />
-      </figure>
-      <Heading>Bem vindo!</Heading>
-      <div className="div-Login">
-        <Button appearance="primary" isLoading={false} onClick={handleLogin}>
-          <Icon marginRight={16} icon="log-in" size={16} />
-          Login
-        </Button>
-      </div>
-    </div>
-    <footer>
-      Copyright &copy; 2019 Catolica - Joinville. All rights reserved.
-    </footer>
-    <style jsx global>
-      {`
-        html,
-        body {
-          height: 100% !important;
-          width: 100%;
-          overflow: hidden;
-          background-color: #f5f5f5;
-        }
-      `}
-    </style>
+  <Pane
+    background="#f5f5f5"
+    height="100vh"
+    width="100%"
+    display="flex"
+    flexDirection="column"
+    alignItems="center"
+    justifyContent="center"
+  >
+    <Pane
+      display="flex"
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <img src="/static/logo.png" alt="Logo" />
+      <Heading size={700} color="#425A70">
+        Restaurante Popular
+      </Heading>
+    </Pane>
+
+    <Pane
+      background="#fff"
+      width="25%"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="space-between"
+      borderRadius={8}
+      padding={40}
+      marginY={80}
+    >
+      <Heading size={600} color="#425A70">
+        Bem vindo!
+      </Heading>
+
+      <Button
+        appearance="primary"
+        height={35}
+        marginTop={50}
+        iconBefore="log-in"
+        isLoading={false}
+        onClick={handleLogin}
+      >
+        ENTRAR
+      </Button>
+    </Pane>
+
+    <Heading size={200} color="#C7CED4">
+      Copyright &copy; {YEAR} Católica - Joinville | Todos os direitos
+      reservados.
+    </Heading>
+
     <style jsx>
       {`
-        .div-Login {
-          margin-top: 10vh;
-          text-align: center;
-        }
-        .div-Center {
-          display: flex;
-          border-radius: 3rem;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          vertical-align: middle;
-          background-color: white;
-          margin: 25vh 25vw;
-          height: 40vh;
-          width: 25vw;
-        }
-        .container {
-          width: 100%;
-          height: 100%;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          min-height: 100%;
-          background-color: #f5f5f5;
+        img {
+          height: 70px;
+          width: 70px;
+          margin-right: 20px;
         }
       `}
     </style>
-  </div>
+  </Pane>
 );
 
 export default Login;
