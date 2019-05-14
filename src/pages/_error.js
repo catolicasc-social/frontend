d554fd52168b12d92702fc2bea7115e09b4b57b6
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-05-14 08:31:56 -0300

    Mehorando tela de erro

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,11 +1,10 @@
 /* eslint-disable react/no-deprecated */
-
 import React from 'react';
-import Link from 'next/link';
-import { withRouter } from 'next/router';
+import Router, { withRouter } from 'next/router';
 
-import { Heading } from '../components/commons/Heading'
-import { Pane } from '../components/commons/Pane'
+import { Button } from '../components/commons/Button';
+import { Heading } from '../components/commons/Heading';
+import { Pane } from '../components/commons/Pane';
 
 class ErrorPage extends React.Component {
   static propTypes() {
@@ -20,77 +19,48 @@ class ErrorPage extends React.Component {
     return { errorCode };
   }
 
+  handleBack() {
+    Router.push('/login');
+  }
+
   render() {
     let response;
     switch (this.props.errorCode) {
       case 200:
       case 404:
         response = (
-          <Pane>
-            <Pane justifyContent="center" alignItems="center" display="flex" marginTop={100}>
-              <Heading color="#bbb">Página não encontrada</Heading>
-            </Pane>
-            <section className="error-container">
-              <Heading size={900} color="#e27b7e">{this.props.errorCode}</Heading>
-            </section>
-            <Pane justifyContent="center" alignItems="center" display="flex">
-              <Link href="/">
-                <a href="home" className='home-link'>Página Inicial</a>
-              </Link>
-            </Pane>
-            <style jsx global>
-              {`
-                html, body {
-                  height: 100%;
-                  overflow: hidden;
-                  background-color: #FDFAF5;
-                }
-              `}
-            </style>
-            <style jsx>
-              {`
-              
-              .error-container {
-                text-align: center;
-                font-size: 106px;
-                font-weight: 800;
-                margin: 70px 15px;
-                color: #e27b7e;
-              }
-              .screen-reader-text {
-                  color: #e27b7e;
-                  font-size: 21vh;
-              } 
-              .not-found {
-                  font-family: 'Montserrat', Helvetica, sans-serif;
-                  color: #bbb;
-                  text-align: center;
-                  margin: 30px 15px;
-              }
-              .zoom-area { 
-                max-width: 490px;
-                margin: 30px auto 30px;
-                font-size: 19px;
-                text-align: center;
-              }
-              a.home-link {
-                text-transform: uppercase;
-                font-size: 13px;
-                background-color: #de7e85;
-                padding: 10px 15px;
-                border-radius: 0;
-                color: #fff;
-                display: inline-block;
-                margin-right: 5px;
-                margin-bottom: 5px;
-                line-height: 1.5;
-                text-decoration: none;
-                margin-top: 50px;
-                letter-spacing: 1px;
-                border-radius: 0.5rem;
-              }
-            `}
-            </style>
+          <Pane
+            background="#f5f5f5"
+            height="100vh"
+            width="100%"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Heading size={800} color="#425A70">
+              Página não encontrada
+            </Heading>
+
+            <Heading
+              size={700}
+              color="#EC4C47"
+              marginTop={20}
+              marginBottom={100}
+            >
+              {this.props.errorCode}
+            </Heading>
+
+            <Button
+              appearance="primary"
+              height={35}
+              width={200}
+              justifyContent="center"
+              isLoading={false}
+              onClick={this.handleBack}
+            >
+              PÁGINA INICIAL
+            </Button>
           </Pane>
         );
         break;
