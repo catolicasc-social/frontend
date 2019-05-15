301adc11e061a301f1d8326db852c069adf51ab5
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-05-14 22:00:12 -0300

    Refatorando tela error (#84)

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,99 +1,94 @@
-/* eslint-disable react/no-deprecated */
 import React from 'react';
-import Router, { withRouter } from 'next/router';
+import Router from 'next/router';
 
 import { Button } from '../components/commons/Button';
+import { Text } from '../components/commons/Text';
+import { Paragraph } from '../components/commons/Paragraph';
 import { Heading } from '../components/commons/Heading';
 import { Pane } from '../components/commons/Pane';
 
-class ErrorPage extends React.Component {
-  static propTypes() {
-    return {
-      errorCode: React.PropTypes.number.isRequired,
-      url: React.PropTypes.string.isRequired
-    };
+const errors = {
+  404: {
+    title: 'Oooops! Página não encontrada',
+    description: 'Parece que o endereço foi digitado incorretamente',
+    statusCode: 404
+  },
+  500: {
+    title: 'Erro do Servidor Interno',
+    description: 'Ocorreu um erro interno no servidor',
+    statusCode: 500
+  },
+  default: {
+    title: 'Erro HTTP',
+    description: 'Ocorreu um erro ao tentar acessar a página'
   }
+};
 
+class Error extends React.Component {
   static getInitialProps({ res, xhr }) {
-    const errorCode = res ? res.statusCode : xhr ? xhr.status : null;
-    return { errorCode };
+    const statusCode = res ? res.statusCode : xhr ? xhr.status : null;
+    return { statusCode };
   }
 
   handleBack() {
     Router.push('/login');
   }
 
+  renderContent({ title, description, statusCode }) {
+    return (
+      <Pane
+        background="#f5f5f5"
+        height="100vh"
+        width="100%"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Heading size={800} color="#425A70">
+          {title}
+          <Text size={600} color="muted" marginLeft={10}>
+            {statusCode}
+          </Text>
+        </Heading>
+
+        <Paragraph size={600} color="#425A70" marginTop={10} marginBottom={50}>
+          {description}
+        </Paragraph>
+
+        <Button
+          appearance="primary"
+          height={35}
+          width={200}
+          justifyContent="center"
+          isLoading={false}
+          onClick={this.handleBack}
+        >
+          PÁGINA INICIAL
+        </Button>
+      </Pane>
+    );
+  }
+
   render() {
     let response;
-    switch (this.props.errorCode) {
+    switch (this.props.statusCode) {
       case 200:
       case 404:
-        response = (
-          <Pane
-            background="#f5f5f5"
-            height="100vh"
-            width="100%"
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-          >
-            <Heading size={800} color="#425A70">
-              Página não encontrada
-            </Heading>
-
-            <Heading
-              size={700}
-              color="#EC4C47"
-              marginTop={20}
-              marginBottom={100}
-            >
-              {this.props.errorCode}
-            </Heading>
-
-            <Button
-              appearance="primary"
-              height={35}
-              width={200}
-              justifyContent="center"
-              isLoading={false}
-              onClick={this.handleBack}
-            >
-              PÁGINA INICIAL
-            </Button>
-          </Pane>
-        );
+        response = this.renderContent(errors[404]);
         break;
       case 500:
-        response = (
-          <div>
-            <h1 className="display-4">Internal Server Error</h1>
-            <p>An internal server error occurred.</p>
-          </div>
-        );
+        response = this.renderContent(errors[500]);
         break;
       default:
-        response = (
-          <div>
-            <h1 className="display-4">
-              HTTP
-              {this.props.errorCode} Error
-            </h1>
-            <p>
-              An
-              <strong>
-                HTTP
-                {this.props.errorCode}
-              </strong>{' '}
-              error occurred while trying to access
-              <strong>{this.props.router.pathname}</strong>
-            </p>
-          </div>
-        );
+        response = this.renderContent({
+          ...errors.default,
+          statusCode: this.props.statusCode
+        });
     }
 
     return response;
   }
 }
 
-export default withRouter(ErrorPage);
+export default Error;
