a2c57b476f1d87c31f7030cf40fdcfda8c136a78
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-05-15 08:22:36 -0300

    Adicionando testes

--- src/components/commons/Filepicker.js
+++
@@ -1,6 +0,0 @@
-import { FilePicker } from 'evergreen-ui';
-
-const FilePickerCustom = ({ ...props }) => <FilePicker {...props} />;
-
-// eslint-disable-next-line import/prefer-default-export
-export { FilePickerCustom as FilePicker };
---
+++ src/components/commons/__tests__/Pane.test.js
@@ -0,0 +1,52 @@
+import { render, fireEvent } from 'react-testing-library';
+
+import { Pane } from '../Pane';
+
+const PRODUTOS = 'Produtos';
+
+describe('Alerta deve', () => {
+  test('renderizar', () => {
+    const { container } = render(<Pane />);
+
+    expect(container).toBeInTheDocument();
+  });
+
+  test('ter node filho', () => {
+    const { getByText } = render(<Pane>{PRODUTOS}</Pane>);
+
+    expect(getByText(PRODUTOS)).toBeInTheDocument();
+  });
+
+  test('ter cor', () => {
+    const { getByText } = render(<Pane color="#425A70">{PRODUTOS}</Pane>);
+
+    expect(getByText(PRODUTOS)).toHaveStyle('color: #425A70;');
+  });
+
+  test('ter altura', () => {
+    const { getByText } = render(<Pane height={48}>{PRODUTOS}</Pane>);
+
+    expect(getByText(PRODUTOS)).toHaveStyle('height: 48px;');
+  });
+
+  test('ter largura', () => {
+    const { getByText } = render(<Pane width={100}>{PRODUTOS}</Pane>);
+
+    expect(getByText(PRODUTOS)).toHaveStyle('width: 100px;');
+  });
+
+  test('ter cor de fundo', () => {
+    const { getByText } = render(<Pane backgroundColor="red">{PRODUTOS}</Pane>);
+
+    expect(getByText(PRODUTOS)).toHaveStyle('background-color: red;');
+  });
+
+  test('ser uma vez clicado', () => {
+    const onClick = jest.fn();
+    const { getByText } = render(<Pane onClick={onClick}>{PRODUTOS}</Pane>);
+
+    fireEvent.click(getByText(PRODUTOS));
+
+    expect(onClick).toHaveBeenCalled();
+  });
+});
