35e608a4c7ae3ba4014a60fc8153b1df400a1b11
lucaspoffo <35241085+lucaspoffo@users.noreply.github.com>
2019-05-24 22:31:56 -0300

    Adicionado componente Table (#97)

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,5 +1,33 @@
+import { IconButton } from 'evergreen-ui';
 import Page from '../layouts/page';
+import Table from '../components/commons/Table';
 
-const Products = () => <Page title="Produtos">produto</Page>;
+const Products = () => {
+  const products = [];
+  for (let i = 0; i < 15; i += 1) {
+    products.push({
+      id: i,
+      name: `teste${i}`,
+      category: `categoria${i}`
+    });
+  }
+
+  const header = { id: 'ID', name: 'Nome', category: 'Categoria' };
+
+  function renderHeader2() {
+    return <IconButton flex={1} icon="plus" />;
+  }
+
+  return (
+    <Page title="Produto">
+      <Table
+        items={products}
+        header={header}
+        tableHeight="calc(100vh - 225px)"
+        header2={renderHeader2()}
+      />
+    </Page>
+  );
+};
 
 export default Products;
